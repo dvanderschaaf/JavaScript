1bc32b604d3d6451bba855fe9d4db8a25107f3fc
Dan Vander Schaaf <dvanderschaaf27@gmail.com>
2017-11-24 15:27:50 -0600

    Display Below Final
    Final to display entered string below.

---
+++ mod3Display.js
@@ -0,0 +1,4 @@
+function displayBelow() {
+  //take the text in the input box and place in the paragragh below with the id stringToDisplay
+  document.getElementById('displayHere').innerHTML = (document.getElementById("stringToDisplay").value);
+};
